6057a020a53182ed661c899a52dc4c88a34cbc1d
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-05-21 11:15:31 +0800

    refactor: immutable Author

--- komga/src/main/kotlin/org/gotson/komga/domain/model/Author.kt
+++ komga/src/main/kotlin/org/gotson/komga/domain/model/Author.kt
@@ -1,26 +1,11 @@
 package org.gotson.komga.domain.model
 
-import javax.validation.constraints.NotBlank
-
-class Author {
-  constructor(name: String, role: String) {
-    this.name = name
-    this.role = role
-  }
-
-  @NotBlank
-  var name: String
-    set(value) {
-      require(value.isNotBlank()) { "name must not be blank" }
-      field = value.trim()
-    }
-
-  @NotBlank
-  var role: String
-    set(value) {
-      require(value.isNotBlank()) { "role must not be blank" }
-      field = value.trim().toLowerCase()
-    }
+class Author(
+  name: String,
+  role: String
+) {
+  val name = name.trim()
+  val role = role.trim().toLowerCase()
 
   override fun toString(): String = "Author($name, $role)"
 }
